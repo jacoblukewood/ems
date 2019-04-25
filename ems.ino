05b1ef8336bf263684bdf5cb9a099441a7aa9b8a
Jacob Wood <jacoblukewood@jambp.local>
2019-04-25 20:14:00 +0800

    Cleaned up code - Everything working - Brake/tail light is set to use internal pinup.

--- ems.ino
+++ ems.ino
@@ -11,10 +11,10 @@
 #define OFF_HOLD_TIME             5000   // * CONFIGURABLE * The length of time the start button should be held to turn the engine off.
 #define DEBOUNCE_DELAY            150    // * CONFIGURABLE * Time between button presses being recorded and being treated as new (the lower the better - increase if flickering occurs or if stop button ceases to work).
 #define FLASH_RATE                120    // * CONFIGURABLE * Times per minute to flash indicators (legally between 60 and 120).
-#define BRAKE_LIGHT_BRIGHTNESS    100    // * CONFIGURABLE * Percentage of tail light brightness to be used when the break light is on.
-#define TAIL_LIGHT_BRIGHTNESS     100    // * CONFIGURABLE * Percentage of tail light brightness to be used when the brake light is not on/
+#define BRAKE_LIGHT_BRIGHTNESS    255    // * CONFIGURABLE * A brightness value between 0 and 255 to be used for the  tail light brightness when the break light is on.
+#define TAIL_LIGHT_BRIGHTNESS     20     // * CONFIGURABLE * A brightness value between 0 and 255 to be used for the  tail light brightness when the break light is off.
 #define MOMENTARY                 0x0    // Used in record_state() to specify that the state variable should only be changed temporally
-#define STICKY                    0x1    // Used in record_state() to specify that the state variable should be changed until the button is pushed again
+#define TOGGLE                    0x1    // Used in record_state() to specify that the state variable should be changed until the button is pushed again
 #define MILLISECONDS_PER_SECOND   1000
 #define MILLISECONDS_PER_MINUTE   60000
 #define MILLISECONDS_PER_HOUR	    3600000
@@ -26,14 +26,14 @@ static int const FLASH_CYCLE    = MILLISECONDS_PER_MINUTE /(FLASH_RATE * 2); // 
 /*
  * Inputs
  */
-static int const high_beam_button_pin       = 4;  // High Beam Button - Sticky
-static int const tail_light_button_pin      = 5;  // Tail Light Button - Momentary
-static int const indicator_right_button_pin = 6;  // Right Indicator Button - Sticky
-static int const indicator_left_button_pin  = 7;  // Left Indicator Button - Sticky
-static int const start_button_pin           = 2;  // Start Button - Momentary
+static int const high_beam_button_pin       = 4;  // High Beam Button - TOGGLE
+static int const tail_light_button_pin      = 5;  // Tail Light Button - MOMENTARY
+static int const indicator_right_button_pin = 6;  // Right Indicator Button - TOGGLE
+static int const indicator_left_button_pin  = 7;  // Left Indicator Button - TOGGLE
+static int const start_button_pin           = 2;  // Start Button - MOMENTARY
 static int const tachometer_pin             = A0; // Tachometer Analogue Input
 static int const speedometer_pin            = 3; // Speedometer Digital Input - Must be a valid interrupt pin (https://www.arduino.cc/reference/en/language/functions/external-interrupts/attachinterrupt/)
-static int const horn_button_pin            = A3;  // Horn Button - Momentary
+static int const horn_button_pin            = A3;  // Horn Button - MOMENTARY
 static int const speedometer_adjustment_pin = A4;
 
 /*
@@ -66,15 +66,12 @@ static int points_state                 = LOW;
 static unsigned long record_state_last_debounce_time                    = 0;
 static unsigned long indicator_trigger_left_indicator_last_flash_time   = 0;
 static unsigned long indicator_trigger_right_indicator_last_flash_time  = 0;
-static unsigned long start_attempt_time                                 = 0;
 static unsigned long stop_bike_initial_push                             = 0;
 static unsigned long stop_bike_last_push_time                           = 0;
 static unsigned long time_last_revoloution                              = 0;
 static unsigned int  previous_speed									                  	= 0;
-static unsigned int  junk_int                                           = 0;
 static unsigned long milliseconds_for_revoloution                       = 0;
-static double calibrated_wheel_circumference_in_kilometers       = 0;
-
+static double calibrated_wheel_circumference_in_kilometers              = 0;
 static unsigned long time_last_speed_calculation                        = 0;
 
 /*
@@ -83,13 +80,12 @@ static unsigned long time_last_speed_calculation                        = 0;
 static void record_state(int read_pin, int output_state_variable, int stickiness);
 static void accessories(int state);
 static void indicator_trigger(int output_state_variable, int output_pin, int contrasting_output_state_variable, unsigned long last_flash_time);
-static void brake_light_trigger(int *button_state, const int *light_pin);
 static void start_bike(void);
 static void stop_bike(void);
 static int bike_running(void);
 static float read_tachometer(const int tachometer_pin);
 static int interval_passed(unsigned long start_time, int interval);
-static unsigned int calculate_speed();
+static unsigned int calculate_speed(unsigned long time_last_revoloution, unsigned long milliseconds_for_revoloution);
 
 
 void setup()
@@ -105,7 +101,7 @@ void setup()
    * Input Pin Setup
    */
   pinMode(high_beam_button_pin       ,INPUT);
-  pinMode(tail_light_button_pin      ,INPUT);
+  pinMode(tail_light_button_pin      ,INPUT_PULLUP);
   pinMode(indicator_right_button_pin ,INPUT);
   pinMode(indicator_left_button_pin  ,INPUT);
   pinMode(start_button_pin           ,INPUT);
@@ -124,7 +120,7 @@ void setup()
   pinMode(starter_motor_pin          ,OUTPUT);
   pinMode(aux_output_pin             ,OUTPUT);
 
-  calibrated_wheel_circumference_in_kilometers = (AVERAGE_WHEEL_CIRCUMFEFRENCE_IN_KILOMETERS * 0.9) + ((analogRead(speedometer_adjustment_pin) * (1.0 / 1023.0)) * (AVERAGE_WHEEL_CIRCUMFEFRENCE_IN_KILOMETERS * 0.2)); // (Calculate the minimum - 90% (0.9) of the average)   (Add a percentage of the difference between the minimum and maximum value - 20% (0.2))
+  calibrated_wheel_circumference_in_kilometers = map(analogRead(speedometer_adjustment_pin), 0, 1023, (AVERAGE_WHEEL_CIRCUMFEFRENCE_IN_KILOMETERS * 0.9), (AVERAGE_WHEEL_CIRCUMFEFRENCE_IN_KILOMETERS * 1.1));
 }
 
 
@@ -134,22 +130,27 @@ void loop()
    * Regardless of Running State
    */    
   /* Record & Action Button States */
-  record_state(high_beam_button_pin, &high_beam_button_state, STICKY);
+  record_state(high_beam_button_pin, &high_beam_button_state, TOGGLE);
   digitalWrite(high_beam_light_pin, high_beam_button_state);
 
   record_state(horn_button_pin, &horn_button_state, MOMENTARY);
   digitalWrite(horn_pin, horn_button_state);
 
   record_state(tail_light_button_pin, &tail_light_button_state, MOMENTARY);
-  brake_light_trigger(&tail_light_button_state, &tail_light_light_pin);
+  if (tail_light_button_state == HIGH) { // Internal Pullup is used so logic is flipped (LOW = Pressed)
+    analogWrite(tail_light_light_pin, BRAKE_LIGHT_BRIGHTNESS);
+  }
+  else {
+    analogWrite(tail_light_light_pin, TAIL_LIGHT_BRIGHTNESS);
+  }
 
   aux_output_state = HIGH;
   digitalWrite(aux_output_pin, aux_output_state);
 
-  record_state(indicator_right_button_pin, &indicator_right_button_state, STICKY);
+  record_state(indicator_right_button_pin, &indicator_right_button_state, TOGGLE);
   indicator_trigger(indicator_right_button_state, indicator_right_light_pin, &indicator_left_button_state,  &indicator_trigger_right_indicator_last_flash_time);
 
-  record_state(indicator_left_button_pin, &indicator_left_button_state, STICKY);
+  record_state(indicator_left_button_pin, &indicator_left_button_state, TOGGLE);
   indicator_trigger(indicator_left_button_state,  indicator_left_light_pin,  &indicator_right_button_state, &indicator_trigger_left_indicator_last_flash_time);
 
   /*
@@ -172,7 +173,7 @@ void loop()
     // }
 
     Serial.print("Speed: ");
-    Serial.print(calculate_speed());
+    Serial.print(calculate_speed(time_last_revoloution, milliseconds_for_revoloution));
     Serial.print(" km/h\n");
 
     // Serial.print("tachometer: ");
@@ -233,25 +234,25 @@ void loop()
 /*
  * Function:  record_state 
  * --------------------
- * Reads the state of a button and either momentary or semi-permentantly (STICKY) assigns the changes to the state variable.
+ * Reads the state of a button and either MOMENTARY or semi-permentantly (TOGGLE) assigns the changes to the state variable.
  *
  * read_pin: the input pin of the button to read.
  *
  * output_state_variable: a reference to the button's corresponding state variable.
  *
- * stickiness: either MOMENTARY (state changes back to LOW when released) or STICKY (holds it's HIGH position until pressed again).
+ * stickiness: either MOMENTARY (state changes back to LOW when released) or TOGGLE (holds it's HIGH position until pressed again).
  */
 static void record_state(int read_pin, int *output_state_variable, int stickiness)
 {
   /* Button Pressed */
   if (digitalRead(read_pin) == HIGH){
-    /* Momentary */
+    /* MOMENTARY */
     if (stickiness == MOMENTARY) {
       *output_state_variable = HIGH;
     }
     
-    /* Sticky */
-    else if (stickiness == STICKY) {
+    /* TOGGLE */
+    else if (stickiness == TOGGLE) {
       if (interval_passed(record_state_last_debounce_time, DEBOUNCE_DELAY))  { // Check if DEBOUNCE_DELAY is exceeded ensure it is a new button press and not noise.
         record_state_last_debounce_time = millis();
         *output_state_variable = !*output_state_variable;
@@ -318,22 +319,6 @@ static void indicator_trigger(int output_state_variable, int output_pin, int *co
   }
 }
 
-
-/*
- * Function:  brake_light_trigger 
- * --------------------
- * Handles the 2-in-1 tail light by using PWM to change it from a tail light to a brake light with brightness.
- */
-static void brake_light_trigger(int *button_state, const int *light_pin) {
-  if (*button_state == HIGH) {
-    analogWrite(*light_pin, BRAKE_LIGHT_BRIGHTNESS / 100 * 255);
-  }
-  else {
-    analogWrite(*light_pin, TAIL_LIGHT_BRIGHTNESS / 100 * 255);
-  }
-}
-
-
 /*
  * Function:  start_bike 
  * --------------------
@@ -343,7 +328,7 @@ static void start_bike(void)
 {
   digitalWrite(points_output, HIGH);
   delay(1000);
-  start_attempt_time = millis();  // Grab current millis() reading to use as a timer for timeout.
+  static unsigned long start_attempt_time = millis();  // Grab current millis() reading to use as a timer for timeout.
   /* While the bike is not running and the starter has not timed out. */
   while (!bike_running() && !interval_passed(start_attempt_time, CRANKING_TIMEOUT)) {
     digitalWrite(starter_motor_pin, HIGH);
@@ -369,7 +354,7 @@ static void stop_bike(void)
   else {                                                  
     stop_bike_last_push_time = millis();
     /* If it's been longer than OFF_HOLD_TIME since the initial push */
-    if (stop_bike_last_push_time > stop_bike_initial_push + OFF_HOLD_TIME && calculate_speed() <= 3) { // If button held and speed is 3km/h or less.
+    if (stop_bike_last_push_time > stop_bike_initial_push + OFF_HOLD_TIME && calculate_speed(time_last_revoloution, milliseconds_for_revoloution) <= 3) { // If button held and speed is 3km/h or less.
       /* Turn the engine off */
       points_state == LOW;
     }
@@ -438,17 +423,12 @@ void read_speedometer_sensor(){
  * 
  * returns: an integer value of the speed in km/h.
  */
-static unsigned int calculate_speed() {
-  if (time_last_speed_calculation < time_last_revoloution){
-    if (milliseconds_for_revoloution > 0) {
-      int speed = calibrated_wheel_circumference_in_kilometers * (MILLISECONDS_PER_HOUR / milliseconds_for_revoloution); // Calculate speed in km/h  
-      if (speed < previous_speed + (time_last_revoloution / MILLISECONDS_PER_SECOND) * MAX_KM_ACCELERATION_PER_SECOND) { // If the speed has accelerated faster than MAX_KM_ACCELERATION_PER_SECOND
-        previous_speed = speed; // Update the speed
-        time_last_speed_calculation = millis();
-      }
-    }
-    else {
-      return 0;
+static unsigned int calculate_speed(unsigned long time_last_revoloution, unsigned long milliseconds_for_revoloution) {
+  if (time_last_speed_calculation < time_last_revoloution) { // If a new revolution has occured
+    int speed = calibrated_wheel_circumference_in_kilometers * (MILLISECONDS_PER_HOUR / milliseconds_for_revoloution); // Calculate speed in km/h  
+    if (speed < previous_speed + (time_last_revoloution / MILLISECONDS_PER_SECOND) * MAX_KM_ACCELERATION_PER_SECOND) { // If the speed has accelerated less than MAX_KM_ACCELERATION_PER_SECOND
+      previous_speed = speed; // Update the speed
+      time_last_speed_calculation = millis();
     }
   }
   else if (interval_passed(time_last_revoloution, (MILLISECONDS_PER_HOUR / ((previous_speed - 1) / calibrated_wheel_circumference_in_kilometers))) && previous_speed > 0) {
